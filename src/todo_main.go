e7e3ab81d1b1ee12eae078db71a883196642a527
YoonJungBoo <coma333ryu@gmail.com>
2015-01-28 17:07:51 +0900

    test

--- src/todo_main.go
+++ src/todo_main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"text/template"
+)
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	mainTemplate, _ := template.ParseFiles("/Users/yoonjeongbu/coma/dev/go-todo/www/html/todo_main.html")
+	mainTemplate.Execute(w, nil)
+}
 
 func main() {
-	fmt.Println("Hello")
+	//	fmt.Println("Hello")
+	http.Handle("/www/", http.StripPrefix("/www/", http.FileServer(http.Dir("."))))
+	http.HandleFunc("/", mainPage)
+	http.ListenAndServe(":8080", nil)
 }
